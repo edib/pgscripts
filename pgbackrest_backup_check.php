12bb4c62309fb054d59465515dca5dcb8b99b6a7
edib <kokdemir@gmail.com>
2019-03-10 19:28:06 +0300

    documentation is added.

--- pgbackrest_backup_check.php
+++ pgbackrest_backup_check.php
@@ -1,5 +1,36 @@
 <?php
-if ($argc < 4) exit("There must be a parameter.\n");
+if ($argc < 4):
+  echo "
+  #===============================================================================
+  #
+  #          FILE:  pgbackrest_check.php
+  #
+  #         USAGE:  php ./pgbackrest_check.sh DATABASEHOST STANZA [time|wal|okcheck|label]
+  #
+  #   DESCRIPTION: This script is to check pgbackrest stanza information and is designed to be used in Zabbix agent UserParameter and can be used externally.
+  #                 Zabbix integration documentation is not included.
+  #
+  #       OPTIONS:
+  #  REQUIREMENTS: 1. This script must run on pgbackrest server.
+  #                2. System must have php-cli, php-pgsql and must access to the pgsql databases on which the arcive commands run.
+  #          BUGS:  ---
+  #         NOTES: Script gets the different information about each stanza. For each information script must run again.
+  #                1st parameter is the host that postgres database runs on.
+  #                2nd param is your stanza name and
+  #                3rd parameter is one of the followings:
+  #                time : how many seconds late the backup is.
+  #                wal: how many wal files late the backup is.
+  #                okcheck: whether the backup is in failed state.
+  #                label: it is intended for zabbix usage to create a trigger and check daily backup success.
+  #        AUTHOR:  ibrahim edib kökdemir, kokdemir@gmail.com
+  #       COMPANY:  Tübitak YTE
+  #       VERSION:  1.0
+  #       CREATED:  2018-02-28 17:24:00 +03
+  #      REVISION:  2019-02-15 22:19:07 +03
+  #===============================================================================
+  ";
+  exit;
+endif;
 
 // return all backups and convert to an object
 $pgb_info = json_decode(shell_exec('pgbackrest info --stanza '.argv[2].' --output json'));
