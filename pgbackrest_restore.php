9227dabadedf8ceacf34725b9e68f87ec375f85e
edib <kokdemir@gmail.com>
2019-03-11 14:58:59 +0300

    documentation is added.

--- pgbackrest_restore.php
+++ pgbackrest_restore.php
@@ -1,18 +1,51 @@
 <?php
-//define necessary parameters
+
+if ($argc < 4):
+  echo "
+  #===============================================================================
+  #
+  #          FILE:  pgbackrest_restore.php
+  #
+  #         USAGE:  ./pgbackrest_restore.php
+  #
+  #   DESCRIPTION: This script is intended to restore pgbackrest stanza to some local directory that predefined in this file. Be careful to set the directory parameters.
+  #
+  #       OPTIONS:
+  #  REQUIREMENTS: 1. PARAMS parameters must be set before running the script.
+  #          BUGS:  ---
+  #         NOTES: Script has two path:
+  #                1. PITR: restore to some point in backup history. For that, you must choose backup set that shown in the menu and restore time after that backup set.
+  #                2. or you may choose to restore to the latest point.
+  #        AUTHOR:  ibrahim edib kökdemir, kokdemir@gmail.com
+  #       COMPANY:  Tübitak YTE
+  #       VERSION:  0.2
+  #       CREATED:  2018-01-29 11:12:45 +03
+  #      REVISION:  2019-03-11 14:58:01 +03
+  #===============================================================================
+  ";
+  exit;
+endif;
+
+//define necessary parameters (following parameters are for redhat distros)
 define('PARAMS',array(
   'PG_CTL' => "/usr/pgsql-11/bin/pg_ctl",
-  'PG_RESTORE_DIR' => "/var/lib/postgresql/11/test",
+  'PG_RESTORE_DIR' => "/var/lib/pgsql/11/test",
   'PG_RESTORE_PORT' => "5555",
   'STANZA_NAME' => "test"
 ));
 
+/*
+to check the given datetime is valid
+*/
 function validateDate($date, $format = 'd-m-Y H:i:s')
 {
     $d = DateTime::createFromFormat($format, $date);
     return $d && $d->format($format) == $date;
 }
 
+/*
+to force to select a date.
+*/
 function timeReadLine($all_backups, $selected) {
   do {
   echo "Select a time to restore, or press enter the default time \n [".date("d-m-Y H:i:s", $all_backups[$selected]->timestamp->stop)."]";
